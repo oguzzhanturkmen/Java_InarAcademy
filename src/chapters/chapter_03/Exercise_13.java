ff61a33d386b113a89b6600909e3024778de6e88
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-08-26 22:01:50 +0300

    Added chapter_03 exercises

--- src/chapters/chapter_03/Exercise_13.java
+++ src/chapters/chapter_03/Exercise_13.java
@@ -5,15 +5,6 @@ import java.util.Scanner;
 public class Exercise_13 {
 	public static void main(String[] args) {
 
-		
-		
-		//				NOT FINISHED YET
-		
-		
-		
-		
-		
-		
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("(0-single filer, 1-married jointly or "
@@ -22,8 +13,8 @@ public class Exercise_13 {
 
 		System.out.print("Enter the taxable income: ");
 		double income = input.nextDouble();
-
 		double tax = 0;
+
 		if (status == 0) {
 			if (income <= 8350)
 				tax = income * 0.10;
@@ -45,30 +36,57 @@ public class Exercise_13 {
 			else if (income <= 67900)
 				tax = 16700 * 0.10 + (income - 16700) * 0.15;
 			else if (income <= 137050)
-				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 137050) * 0.25;
-			else if (income <= 208.850)
-				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137.050 - 67.900) * 0.25 + (income - 208.850) * 0.28;
+				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (income - 67900) * 0.25;
+			else if (income <= 208850)
+				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (income - 137051) * 0.28;
 			else if (income <= 372950)
-				tax = 16.700 * 0.10 + (67900 - 16700) * 0.15 + (137.050 - 67.900) * 0.25 + (208.850 - 137.050) * 0.28
-						+ (income - 372.950) * 0.33;
+				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137051) * 0.28
+						+ (income - 208851) * 0.33;
+			else
+				tax = 16700 * 0.10 + (67900 - 16700) * 0.15 + (137050 - 67900) * 0.25 + (208850 - 137051) * 0.28
+						+ (372950 - 208851) * 0.33 + (income - 372950) * 0.35;
+			
 
 		} else if (status == 2) {
 			if (income <= 8350)
 				tax = income * 0.10;
 			else if (income <= 33950)
 				tax = 8350 * 0.10 + (income - 8350) * 0.15;
-			else if (income <= 68.525)
+			else if (income <= 68525)
 				tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25;
+			else if (income <= 104425)
+				tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (income - 68525) * 0.28;
+			else if (income <= 186475)
+				tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (104425 - 68525) * 0.28
+						+ (income - 186475) * 0.33;
+			else
+				tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (68525 - 33950) * 0.25 + (104425 - 68525) * 0.28
+						+ (186475 - 104425) * 0.33 + (income - 186475) * 0.35;
 
 		} else if (status == 3) {
+			if (income <= 11950)
+				tax = income * 0.10;
+			else if (income <= 45500)
+				tax = 11950 * 0.10 + (income - 45500) * 0.15;
+			else if (income <= 117450)
+				tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (income - 117450) * 0.25;
+			else if (income <= 190200)
+				tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (income - 190201) * 0.28;
+			else if (income <= 372950)
+				tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (190201 - 117450) * 0.28
+						+ (income - 372950) * 0.33;
+			else
+				tax = 11950 * 0.10 + (45500 - 11950) * 0.15 + (117450 - 45500) * 0.25 + (190201 - 117450) * 0.28
+						+ (372950 - 190200) * 0.33 + (income - 372951) * 0.35;
 
 		} else {
 			System.out.println("Error: invalid status");
 			System.exit(1);
 		}
-		// Display the result
+
 		System.out.println("Tax is " + (int) (tax * 100) / 100.0);
+	}
 
 	}
 
-}
+
