737a44a8c36db09800752f6faba9469548ea264f
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-09-10 03:01:26 +0300

    added chapter_05 exercises

---
+++ src/chapters/chapter_05/Exercise_02.java
@@ -0,0 +1,13 @@
+package chapters.chapter_05;
+
+import java.util.Scanner;
+
+public class Exercise_02 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+	}
+
+}
